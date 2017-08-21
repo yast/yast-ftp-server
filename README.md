5f319b661c64991a0fe3402fa6d303a1c6a7e529
Ancor Gonzalez Sosa <ancor@suse.de>
2017-08-21 16:45:52 +0200

    Add coveralls to the repository

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![Travis Build](https://travis-ci.org/yast/yast-ftp-server.svg?branch=master)](https://travis-ci.org/yast/yast-ftp-server)
 [![Jenkins Build](http://img.shields.io/jenkins/s/https/ci.opensuse.org/yast-ftp-server-master.svg)](https://ci.opensuse.org/view/Yast/job/yast-ftp-server-master/)
-
+[![Coverage Status](https://img.shields.io/coveralls/yast/yast-ftp-server/master.svg)](https://coveralls.io/github/yast/yast-ftp-server?branch=master)
--- test/ftpserver_test.rb
+++ test/ftpserver_test.rb
@@ -4,6 +4,27 @@ ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
 
 require "yast"
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start do
+    # Don't measure coverage of the tests themselves.
+    add_filter "/test/"
+  end
+
+  # track all ruby files under src
+  src_location = File.expand_path("../../src", __FILE__)
+  SimpleCov.track_files("#{src_location}/**/*.rb")
+
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
+end
+
 # stub module to prevent its Import
 # Useful for modules from different yast packages, to avoid build dependencies
 def stub_module(name)
